dc10e2a324da70bf703a792fc03dcc8c7efcf6d6
JakobJohnson <madjzj@gmail.com>
2023-11-15 13:53:06 -0800

    Completed Lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList(props){
+    return <main>
+        {props.posts.map(function(post){
+            return <Article key={post.id}title={post.title} date={post.date} preview={post.preview} />
+        })}
+    </main>
+}
+
+export default ArticleList
